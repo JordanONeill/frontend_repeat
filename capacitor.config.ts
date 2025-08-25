4d2a3a712442c15215d7c64935093bf5be126f30
Jordan <G00416446@atu.ie>
2025-08-25 11:11:34 +0100

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'recipe-explorer',
+  webDir: 'www'
+};
+
+export default config;
