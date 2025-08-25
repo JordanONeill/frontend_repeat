5599313e132095621e2af82ecad5a5a519699587
Jordan <G00416446@atu.ie>
2025-08-25 11:20:29 +0100

    add app routes for home, favorites, my dishes, recipe detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
+import { FavoritesPage } from './favorites/favorites.page';
+import { MyDishesPage } from './my-dishes/my-dishes.page';
+import { RecipeDetailPage } from './recipe-detail/recipe-detail.page';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: '', component: HomePage },
+  { path: 'favorites', component: FavoritesPage },
+  { path: 'my-dishes', component: MyDishesPage },
+  { path: 'recipe/:id', component: RecipeDetailPage },
 ];
