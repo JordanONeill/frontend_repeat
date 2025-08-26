b6e77febb3f1e8a8922aa3c7e62df0c66ccfd015
Jordan <G00416446@atu.ie>
2025-08-26 18:50:01 +0100

    Add home page with search functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+  templateUrl: './home.page.html',
 })
 export class HomePage {
-  constructor() {}
+  query = '';
+  results: any[] = [];
+
+  constructor(private recipeService: RecipeService, private router: Router) {}
+
+  search() {
+    this.recipeService.searchRecipes(this.query).subscribe((data: any) => {
+      this.results = data.meals || [];
+    });
+  }
+
+  openRecipe(id: string) {
+    this.router.navigate(['/recipe', id]);
+  }
 }
